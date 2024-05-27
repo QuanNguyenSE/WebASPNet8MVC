7c390d2f93a09e02acbe7bedcf7d7c8ec146157f
Nguyen Hoang Quan <quan2k3368@gmail.com>
2024-05-27 16:37:16 +0700

    Add and Remove CartItem

---
+++ WebASPNet8MVC/Controllers/CartController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebASPNet8MVC.Data;
+using WebASPNet8MVC.Helpers;
+using WebASPNet8MVC.ViewModels;
+
+namespace WebASPNet8MVC.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly HshopContext _context;
+
+        public CartController(HshopContext context)
+        {
+            _context = context;
+        }
+        const string CART_KEY = "MYCART";
+        public List<CartItem> Cart => HttpContext.Session.Get<List<CartItem>>
+            (CART_KEY) ?? new List<CartItem>();
+        public IActionResult Index()
+        {
+            return View(Cart);
+        }
+        public IActionResult AddToCart(int? Id, int quantity = 1)
+        {
+            var giohang = Cart;
+            var item = giohang.FirstOrDefault(p => p.MaHh == Id);
+            if (item == null)
+            {
+                var data = _context.HangHoas.FirstOrDefault(p => p.MaHh == Id);
+                if (data == null)
+                {
+                    TempData["Message"] = $"Không tìm thấy mã hàng hóa {Id}";
+                    return Redirect("/404");
+
+                }
+                item = new CartItem
+                {
+                    MaHh = data.MaHh,
+                    TenHh = data.TenHh,
+                    Hinh = data.Hinh,
+                    DonGia = data.DonGia ?? 0,
+                    SoLuong = quantity
+
+                };
+                giohang.Add(item);
+            }
+            else
+            {
+                item.SoLuong += quantity;
+            }
+            HttpContext.Session.Set(CART_KEY, giohang);
+            return RedirectToAction("Index");
+        }
+        public IActionResult RemoveCart(int? id)
+        {
+            var giohang = Cart;
+            var item = giohang.FirstOrDefault(p => p.MaHh == id);
+            if (item == null)
+            {
+                var data = _context.HangHoas.FirstOrDefault(p => p.MaHh == id);
+                if (data == null)
+                {
+                    TempData["Message"] = $"Không tìm thấy mã hàng hóa {id}";
+                    return Redirect("/404");
+
+                }
+
+            }
+            giohang.Remove(item);
+
+            HttpContext.Session.Set(CART_KEY, giohang);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- WebASPNet8MVC/Program.cs
+++ WebASPNet8MVC/Program.cs
@@ -10,6 +10,17 @@ builder.Services.AddDbContext<HshopContext>(option =>
 {
     option.UseSqlServer(builder.Configuration.GetConnectionString("myDB"));
 });
+
+//add session
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +35,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseSession();
 
 app.UseAuthorization();
 
